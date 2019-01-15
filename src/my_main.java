0aa31c605fc62bda290bd698d2d24fbf578c91b6
Margarita Murazyan <murazyan.margarita@gmail.com>
2019-01-15 18:42:51 +0400

    Initial commit

---
+++ src/my_main.java
@@ -0,0 +1,26 @@
+import java.io.PrintWriter;
+
+import java.io.PrintWriter;
+public class my_main {
+    public static void main(String[] args) {
+        String download_path="C:\\youtube_db_java";
+        String url="https://www.youtube.com/watch?v=UUpncwQivLM";
+        String[] command =
+                {
+                        "cmd",
+                };
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+            new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+            PrintWriter stdin = new PrintWriter(p.getOutputStream());
+            stdin.println("cd \""+download_path+"\"");
+            stdin.println(download_path+"\\youtube-dl "+url);
+            stdin.close();
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
